335e3673240b7e13d70c95219c76c501ff72fea8
ToastXC <100072983+toastxc@users.noreply.github.com>
2023-04-22 19:48:32 +0800

    added testing support for selfbot

--- src/tests/common.rs
+++ src/tests/common.rs
@@ -1,16 +1,34 @@
 use crate::methods::driver::Delta;
-
+pub const SERVER: &str = "01GKWVWGHN242DVWG4BKXG2C7F";
+pub const USER: &str = "01GXF9E5H7K6BSJ6Q9QGWYRVWD";
+pub const ROLE: &str = "01GXFR9FPEPFY188X5MKV2E8ZN";
+pub const CHANNEL: &str = "01GKWVWGHNBNCFPC9Q7CRDHBVZ";
+pub const GROUP: &str = "01GYM0JBNKWRJYX56F9GYABS4R";
 // enter values here for testing
-pub async fn tester() -> Delta {
+
+pub async fn tester_bot() -> Delta {
+    Delta::new(
+        // url
+        "https://api.revolt.chat/",
+        // token
+        include_str!("bot-token.txt"),
+        // timeout in seconds
+        10,
+        // is bot account
+        true,
+    )
+}
+// enter values here for testing - with selfbot
+pub async fn tester_user() -> Delta {
     tokio::time::sleep(std::time::Duration::from_secs(1)).await;
     Delta::new(
         // url
         "https://api.revolt.chat/",
         // token
-        include_str!("token.txt"),
+        include_str!("self-token.txt"),
         // timeout in seconds
         10,
         // is bot account
-        true,
+        false,
     )
 }

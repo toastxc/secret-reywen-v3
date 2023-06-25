491c6c15aa914e59cf3c03d66e1f133ab261eed1
ToastXC <100072983+toastxc@users.noreply.github.com>
2023-06-25 11:32:18 +0800

    created dual connect and example of using it

--- src/websocket/process.rs
+++ src/websocket/process.rs
@@ -1,6 +1,9 @@
 use std::pin::Pin;
 
-use futures_util::{Stream, StreamExt};
+use futures_util::{
+    stream::{SplitSink, SplitStream},
+    Stream, StreamExt,
+};
 use tokio_tungstenite::{connect_async, WebSocketStream};
 
 use super::{data::WebSocketEvent, Websocket};
@@ -33,6 +36,37 @@ impl Websocket {
     pub async fn start(self) -> Pin<Box<impl Stream<Item = WebSocketEvent>>> {
         Websocket::stream(self.generate().await).await
     }
+
+    pub async fn new_stream(input: StreamSplit) -> Pin<Box<impl Stream<Item = WebSocketEvent>>> {
+        Box::pin({
+            (input).filter_map(|result| async {
+                match result {
+                    Ok(message) => serde_json::from_slice(&message.into_data()).ok(),
+                    Err(e) => {
+                        println!("{e}");
+                        None
+                    }
+                }
+            })
+        })
+    }
+
+    pub async fn dual_connection(
+        &self,
+    ) -> (SinkSplit, Pin<Box<impl Stream<Item = WebSocketEvent>>>) {
+        let ws = self.clone().generate().await;
+
+        let (write, read) = ws.split();
+
+        (write, Websocket::new_stream(read).await)
+    }
 }
 
 type Connection = WebSocketStream<tokio_tungstenite::MaybeTlsStream<tokio::net::TcpStream>>;
+
+type StreamSplit =
+    SplitStream<WebSocketStream<tokio_tungstenite::MaybeTlsStream<tokio::net::TcpStream>>>;
+type SinkSplit = SplitSink<
+    WebSocketStream<tokio_tungstenite::MaybeTlsStream<tokio::net::TcpStream>>,
+    tokio_tungstenite::tungstenite::Message,
+>;
